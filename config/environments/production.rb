b7a4606d3366e30e9d0a455adcc194200ef37050
Staffan Asami <staffan.a@gmail.com>
2014-10-18 22:06:31 +0200

    Implemeted Devise for Users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ OneMonthPinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  # Required for heroku
+  # Note to set to actual host.
+  config.action_mailer.default_url_options = { :host => 'https://github.com/Staffan-a/OneMonthPinteresting' }
 end
